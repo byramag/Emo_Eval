21246fd9e13f407fb93ff6db9fa0c7d68ff0b6bb
Abigail Byram <byramabigail@gmail.com>
2018-11-25 13:48:59 -0500

    Moved contraction map to separate file

--- emo_eval.py
+++ emo_eval.py
@@ -19,7 +19,7 @@ Author: Abbie Byram
 
 # DEPENDENCIES: sklearn, pandas, pandas_ml, spacy
 
-import re, sys
+import re, sys, json
 from time import time
 import numpy as np
 import pandas as pd
@@ -33,10 +33,9 @@ from sklearn.naive_bayes import GaussianNB, MultinomialNB
 from sklearn.ensemble import RandomForestClassifier
 #from pandas_ml import ConfusionMatrix
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, confusion_matrix
+import emoji, emot
 from nltk.tokenize import TweetTokenizer 
 from nltk.stem import WordNetLemmatizer
-import emoji
-import emot
 import nltk
 nltk.download('wordnet')
 
@@ -56,67 +55,26 @@ def preprocess(instances):
         row_strings.append(instance['turn1'] + ' ' + instance['turn2'] + ' ' + instance['turn3'])
         labels.append(instance['label'])
     
-        
-    CONTRACTION_MAP = {"ain't": "is not", "aren't": "are not","can't": "cannot", 
-                       "can't've": "cannot have", "'cause": "because", "could've": "could have", 
-                       "couldn't": "could not", "couldn't've": "could not have","didn't": "did not", 
-                       "doesn't": "does not", "don't": "do not", "hadn't": "had not", 
-                       "hadn't've": "had not have", "hasn't": "has not", "haven't": "have not", 
-                       "he'd": "he would", "he'd've": "he would have", "he'll": "he will", 
-                       "he'll've": "he he will have", "he's": "he is", "how'd": "how did", 
-                       "how'd'y": "how do you", "how'll": "how will", "how's": "how is", 
-                       "I'd": "I would", "I'd've": "I would have", "I'll": "I will", 
-                       "I'll've": "I will have","I'm": "I am", "I've": "I have", 
-                       "i'd": "i would", "i'd've": "i would have", "i'll": "i will", 
-                       "i'll've": "i will have","i'm": "i am", "i've": "i have", 
-                       "isn't": "is not", "it'd": "it would", "it'd've": "it would have", 
-                       "it'll": "it will", "it'll've": "it will have","it's": "it is", 
-                       "let's": "let us", "ma'am": "madam", "mayn't": "may not", 
-                       "might've": "might have","mightn't": "might not","mightn't've": "might not have", 
-                       "must've": "must have", "mustn't": "must not", "mustn't've": "must not have", 
-                       "needn't": "need not", "needn't've": "need not have","o'clock": "of the clock", 
-                       "oughtn't": "ought not", "oughtn't've": "ought not have", "shan't": "shall not",
-                       "sha'n't": "shall not", "shan't've": "shall not have", "she'd": "she would", 
-                       "she'd've": "she would have", "she'll": "she will", "she'll've": "she will have", 
-                       "she's": "she is", "should've": "should have", "shouldn't": "should not", 
-                       "shouldn't've": "should not have", "so've": "so have","so's": "so as", 
-                       "this's": "this is",
-                       "that'd": "that would", "that'd've": "that would have","that's": "that is", 
-                       "there'd": "there would", "there'd've": "there would have","there's": "there is", 
-                       "they'd": "they would", "they'd've": "they would have", "they'll": "they will", 
-                       "they'll've": "they will have", "they're": "they are", "they've": "they have", 
-                       "to've": "to have", "wasn't": "was not", "we'd": "we would", 
-                       "we'd've": "we would have", "we'll": "we will", "we'll've": "we will have", 
-                       "we're": "we are", "we've": "we have", "weren't": "were not", 
-                       "what'll": "what will", "what'll've": "what will have", "what're": "what are", 
-                       "what's": "what is", "what've": "what have", "when's": "when is", 
-                       "when've": "when have", "where'd": "where did", "where's": "where is", 
-                       "where've": "where have", "who'll": "who will", "who'll've": "who will have", 
-                       "who's": "who is", "who've": "who have", "why's": "why is", 
-                       "why've": "why have", "will've": "will have", "won't": "will not", 
-                       "won't've": "will not have", "would've": "would have", "wouldn't": "would not", 
-                       "wouldn't've": "would not have", "y'all": "you all", "y'all'd": "you all would",
-                       "y'all'd've": "you all would have","y'all're": "you all are","y'all've": "you all have",
-                       "you'd": "you would", "you'd've": "you would have", "you'll": "you will", 
-                       "you'll've": "you will have", "you're": "you are", "you've": "you have" } 
+    with open('abbreviations.json', 'r') as abbr_data: 
+        ABBREVIATION_MAP = json.load(abbr_data)
 
-    #Expands contractions in a sentence according to a contraction_mapping.
-    # expand word contractions (can't -> cannot) -- minor or no impact
+    #Expands abbreviations in a sentence according to a abbreviation_mapping.
+    # expand word contractions (can't -> cannot) and shorthand (lol -> laugh out loud) -- minor or no impact
     
-    def expand_contractions(sentence, contraction_mapping): 
+    def expand_abbreviations(sentence, abbreviation_mapping): 
 
-        contractions_pattern = re.compile('({})'.format('|'.join(contraction_mapping.keys())),  
+        abbreviations_pattern = re.compile('({})'.format('|'.join(abbreviation_mapping.keys())),  
                                           flags=re.IGNORECASE|re.DOTALL) 
-        def expand_match(contraction): 
-            match = contraction.group(0) 
+        def expand_match(abbreviation): 
+            match = abbreviation.group(0) 
             first_char = match[0] 
-            expanded_contraction = contraction_mapping.get(match) if contraction_mapping.get(match) else contraction_mapping.get(match.lower())   
-            if not expanded_contraction:
+            expanded_abbreviation = abbreviation_mapping.get(match) if abbreviation_mapping.get(match) else abbreviation_mapping.get(match.lower())   
+            if not expanded_abbreviation:
                 return match
-            expanded_contraction = first_char+expanded_contraction[1:] 
-            return expanded_contraction
+            expanded_abbreviation = first_char+expanded_abbreviation[1:] 
+            return expanded_abbreviation
 
-        expanded_sentence = contractions_pattern.sub(expand_match, sentence) 
+        expanded_sentence = abbreviations_pattern.sub(expand_match, sentence) 
         return expanded_sentence 
 
 
@@ -131,7 +89,7 @@ def preprocess(instances):
                 text = text.replace(value,':%s:'%'_'.join(mean.split()))
         return text
         
-    row_strings = [desmilify(emoji.demojize(expand_contractions(txt, CONTRACTION_MAP))) for txt in row_strings]
+    row_strings = [desmilify(emoji.demojize(expand_abbreviations(txt, ABBREVIATION_MAP))) for txt in row_strings]
 
     return row_strings, labels
 #Keerthi - Preprocessing end
@@ -154,9 +112,15 @@ def feature_extraction(samples):
 
     tw = TweetTokenizer()
     wnl = WordNetLemmatizer()
-    x_train_mat = TfidfVectorizer(ngram_range=(1,2),min_df=10,max_df=0.95, sublinear_tf=True, norm='l2', smooth_idf=True,tokenizer=lambda x:[wnl.lemmatize(t) for t in tw.tokenize(x)]).fit_transform(samples)
-    
-    #x_train_mat = TfidfVectorizer(ngram_range=(1,2)).fit_transform(samples)
+    x_train_mat = TfidfVectorizer(
+        ngram_range=(1,2),
+        min_df=10,
+        max_df=0.95,
+        sublinear_tf=True,
+        norm='l2', 
+        smooth_idf=True,
+        tokenizer=lambda x:[wnl.lemmatize(t) for t in tw.tokenize(x)]
+    ).fit_transform(samples)
 
     # # Dimensionality reduction - THIS CURRENTLY GIVES BAD RESULTS
     # print("Dimensionality reduction")
@@ -208,7 +172,6 @@ def train_crossval(x_train, y_train, folds=5):
     t = time()
     metrics['SGD'] = cross_validate(sgdClassifier, x_train, y_train, scoring=scoring, cv=folds, return_train_score=False,n_jobs=3)
     print("Training SGD finished in %0.3fsec\n" % (time()-t))
-    
 #Keerthi - SGD classifier end
 
     for model in metrics:
